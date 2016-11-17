f8bdb2267cf56a67572bd73397f63582e3ea928e
Manuel Meyer <manuel@Manuels-MacBook-Pro.local>
2016-11-17 21:38:57 +0100

    Initial Commit

---
+++ HearThis/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HearThis
+//
+//  Created by Manuel Meyer on 17.11.16.
+//  Copyright © 2016 Manuel Meyer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
